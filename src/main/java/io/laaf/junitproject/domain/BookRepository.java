b1edbf2275abb76aa277e3f007639f635487c440
kaka <iamkimseunghyun@gmail.com>
2022-07-18 22:31:52 +0900

    Repository 테스트 환경 세팅 완료

---
+++ src/main/java/io/laaf/junitproject/domain/BookRepository.java
@@ -0,0 +1,8 @@
+package io.laaf.junitproject.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book,Long> {
+}
+
+
---
+++ src/test/java/io/laaf/junitproject/web/BookApiControllerTest.java
@@ -0,0 +1,10 @@
+package io.laaf.junitproject.web;
+
+import io.laaf.junitproject.domain.Book;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookApiControllerTest {
+
+
+}
